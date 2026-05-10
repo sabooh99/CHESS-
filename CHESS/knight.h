061e2c8a2adda4dbcc0f8ffbb9d48dfe9f5f1a4f
aleenaf2 <f250571@cfd.nu.edu.pk>
2026-05-10 23:14:48 +0500

    knight.cpp
    Implemented Knight class with L-shape move validation

---
+++ CHESS/knight.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "base.h"
+
+class Knight : public Piece {
+public:
+    Knight(string colour, int row, int col);
+
+    char   getSymbol() const override;
+    string getName()   const override;
+
+    
+    bool isValidMove(int toRow, int toCol, Board& b) override;
+};
